dc91120458ddb1e013233c213619376ad7273fd6
ElieThellier <eliethellier@gmail.com>
2022-06-22 13:20:57 +0200

    kk

--- main.c
+++
@@ -1,8 +0,0 @@
-#include"point.h"
-#include<stdio.h>
-
-int main(){
-    Point *p = create_Point(5,12);
-    printf("test\n");
-    return 0;
-}
--- point.c
+++
@@ -1,9 +0,0 @@
-#include"point.h"
-#include<stdlib.h>
-
-Point* create_Point(int a, int b){
-    Point *res = malloc(sizeof(Point));
-    res -> x=a;
-    res -> y=b;
-    return res;
-}
